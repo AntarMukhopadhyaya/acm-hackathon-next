39d0bb22a7736c90d13c7b7ce26a02dffa5fb987
Dripto Bhattacharyya <21f3001428@student.onlinedegree.iitm.ac.in>
2024-04-22 01:35:54 +0530

    Almost completed Executive Team page. Included Debmalya's change too

--- app/executive-team/page.tsx
+++ app/executive-team/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { people } from "./PeopleList";
-import Heading from "../components/Heading";
-import Person from "../components/Person";
+import Heading from "./components/Heading";
+import Person from "./components/Person";
 const page = () => {
   return (
     <div className="sm:pt-40 xl:pt-40">
@@ -54,6 +54,25 @@ const page = () => {
                 description={person.description}
                 image={person.image}
                 role={person.role}
+              />
+            ))}
+        </div>
+      </div>
+
+      <Heading title="CORE COMMITTEE" />
+
+      <div className="flex text-center justify-center align-items-center">
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-3 lg:gap-20 p-8 md:p-12 lg:px-16 lg:py-24 lg:mt-10 ">
+          {people
+            .filter((person) => person.role === "Core Committee")
+            .map((person, index) => (
+              <Person
+                key={index}
+                name={person.name}
+                description={person.description}
+                image={person.image}
+                role={person.role}
+                designation={person.designation}
               />
             ))}
         </div>
