f7a33a22762c9f6460ffcc747fa9bf42d996e50d
Debmalya dash <98879060+debmalya02@users.noreply.github.com>
2024-06-25 00:56:47 +0530

    problem statement, animation

--- app/components/ThemeSection.tsx
+++ app/components/ThemeSection.tsx
@@ -104,14 +104,13 @@ function ThemeSection() {
                       </p>
                     </div>
                     <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 flex justify-center mt-2">
-                      <a
-                        className="group inline-block rounded-full bg-gradient-to-r from-[#3F80FF] to-[#5CE1E6] p-[2px] hover:text-[#0F337F] focus:outline-none focus:ring active:text-opacity-75"
-                        href="/problem-statement"
-                      >
-                        <span className="block rounded-full bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent" style={{fontFamily:'Varela'}}>
-                          Know more
-                        </span>
-                      </a>
+                        <Link href={theme.link}
+                          className="group inline-block rounded-full bg-gradient-to-r from-[#3F80FF] to-[#5CE1E6] p-[2px] hover:text-[#0F337F] focus:outline-none focus:ring active:text-opacity-75"
+                        >
+                          <span className="block rounded-full bg-white px-8 py-3 text-sm font-medium group-hover:bg-transparent" style={{fontFamily:'Varela'}}>
+                            Know more
+                          </span>
+                        </Link>
                     </div>
                   </div>
                 </div>
--- app/components/ThemesList.ts
+++ app/components/ThemesList.ts
@@ -1,46 +1,57 @@
 export const themes = [
   {
-    title: "Health Care",
-    image: "/images/Themes/health.png",
-  },
-  {
-    title: "Fitness and Sports",
+    title: "Fitness & Healthcare",
     image: "/images/Themes/fitness.png",
+    link: "/fitness-and-healthcare"
   },
   {
     title: "Smart Education",
     image: "/images/Themes/education.png",
-  },
-  {
-    title: "Fintech",
-    image: "/images/Themes/fintech.png",
+    link: "/smart-education"
   },
   {
     title: "Green Tech",
     image: "/images/Themes/greentech.png",
+    link: "/green-tech"
   },
   {
     title: "Smart Retail",
     image: "/images/Themes/retail.png",
+    link: "/smart-retail"
   },
   {
     title: "Cybersecurity",
     image: "/images/Themes/cybersecurity.png",
+    link: "/cybersecurity"
   },
   {
     title: "Agriculture",
     image: "/images/Themes/agriculture.png",
+    link: "/agriculture"
   },
   {
     title: "Bio-informatics",
     image: "/images/Themes/bio.png",
+    link: "/bio-informatics"
   },
   {
     title: "Smart Automation",
     image: "/images/Themes/automation.png",
+    link: "/smart-automation"
   },
   {
     title: "Open Innovation",
     image: "/images/Themes/innovation.png",
+    link: "/open-innovation"
   },
+  {
+    title: "Social Services",
+    image: "/images/Themes/social.png",
+    link: "/social-service"
+  },
+  {
+    title: "Robotics",
+    image: "/images/Themes/robotics.png",
+    link: "/robotics"
+  }
 ];
