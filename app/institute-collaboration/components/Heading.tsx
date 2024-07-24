af9d64f0acc6934321e04b270eb8b7adf1f1978c
Dripto Bhattacharyya <21f3001428@student.onlinedegree.iitm.ac.in>
2024-07-24 20:31:06 +0530

    Collaborators change, An initiative

--- app/institute-collaboration/components/Heading.tsx
+++ app/institute-collaboration/components/Heading.tsx
@@ -16,6 +16,8 @@ const Heading: React.FC<Props> = ({ title, isMobile }) => {
           {title === "INSTITUTE COLLABORATORS" ? (
             <div>
               <p className="font-bold text-5xl text-[#AAE6FF]">INSTITUTE</p>
+              <p className="font-bold lg:indent-[30%] text-5xl text-white">AND</p>
+              <p className="font-bold text-5xl text-[#AAE6FF]">ACM STUDENT CHAPTER</p>
               <p className="font-bold lg:indent-[30%] text-5xl text-white">
                 {isMobile ? "COLLAB" : "COLLABORATORS"}
               </p>
